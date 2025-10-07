52e3186f0c75396df203559e14f3a940f16fc007
Ubuntu <ubuntu@ip-172-31-17-225.ap-northeast-1.compute.internal>
2025-10-07 15:59:23 +0000

    [Finish] 検索機能

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,13 @@
+class SearchesController < ApplicationController
+  def search
+    @model = params[:model]
+    @method = params[:method]
+    @content = params[:content]
+
+    if @model == "user"
+      @records = User.search_for(@content, @method)
+    else
+      @records = Book.search_for(@content, @method)
+    end
+  end
+end
